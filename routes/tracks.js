4b02421323f81cd23320d4e9ab43453fbeac1322
Your Name <=>
2021-09-28 15:24:22 -0500

    ready for deployment

--- routes/tracks.js
+++ routes/tracks.js
@@ -4,12 +4,20 @@ var musicsCtrl = require('../controllers/musics')
 //Require a track model
 
 /* GET home page. */
-router.get('/',musicsCtrl.index );
+router.get('/', musicsCtrl.index );
 
 
-router.post('/',musicsCtrl.create )
+router.post('/', musicsCtrl.create )
 
 router.put('/:id', musicsCtrl.update)
 router.delete('/:id', musicsCtrl.delete);
+
+
+// function isAuthenticated(req, res, next){
+//     if (!req.query.uid) return res.status(401).json({error: 'user must be Logged in '})
+//     next();
+// }
+
+
 
 module.exports = router;
